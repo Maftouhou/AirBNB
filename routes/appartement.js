f774fd69d90c28798a3c7e978372d17fb624fe7a
Maftouhou Hassani <hassani.maftouhou@ynov.com>
2017-10-08 09:51:40 +0200

    Implementing Http methods for the appartement Web Service

--- routes/appartement.js
+++ routes/appartement.js
@@ -1,7 +1,84 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+var express = require('express');
+var router = express.Router();
+
+var AppartementModel = require('../models/appartementModel');
+
+/**
+ * Get one Appartement
+ * 
+ * @param {Http} req 
+ * @param {Http} res 
+ * @param {Http} next 
  */
+router.get('/:id', function(req, res, next) {
 
+    AppartementModel.findOne({_id: req.params.id}).then(function(appart){
+        res.send(appart);
+    }).catch(next);
+});
 
+/**
+ * Search for spesific Appartement
+ * Or get all Appartement
+ * 
+ * @param {Http} req 
+ * @param {Http} res 
+ * @param {Http} next 
+ */
+router.get('/', function(req, res, next) {
+    if(typeof req.query.appartementName === "string"){
+        AppartementModel.find({appartementName: req.query.appartementName}).then(function(appart){
+            res.send(appart);
+        }).catch(next);
+    }else{
+        AppartementModel.find().then(function(data){
+            res.send(data);
+        }).catch(next);
+    }
+});
+
+/**
+ * Create a Appartement
+ * 
+ * @param {Http} req 
+ * @param {Http} res 
+ * @param {Http} next 
+ */
+router.post('/', function (req, res, next) {
+    
+    AppartementModel.create(req.body).then(function(appart){
+        res.send(appart);
+    }).catch(next);
+});
+
+/**
+ * Update a Appartement
+ * 
+ * @param {Http} req 
+ * @param {Http} res 
+ * @param {Http} next 
+ */
+router.put('/:id', function (req, res, next) {
+    
+    AppartementModel.findByIdAndUpdate({_id: req.params.id}, req.body).then(function(){
+        AppartementModel.findOne({_id: req.params.id}).then(function(appart){
+            res.send(appart);
+        }).catch(next);
+    }).catch(next);
+});
+
+/**
+ * Delete a Appartement
+ * 
+ * @param {Http} req 
+ * @param {Http} res 
+ * @param {Http} next 
+ */
+router.delete('/:id', function (req, res, next) {
+    
+    AppartementModel.findByIdAndRemove({_id: req.params.id}).then(function(appart){
+        res.send(appart);
+    }).catch(next);
+});
+
+module.exports = router;
